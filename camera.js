8c8fccea5c6c2db36844aa349bf07ee7d30f96e2
Mike Tyka <mike.tyka@gmail.com>
2015-11-15 07:55:31 +0000

    Initial working projection and camera module.

---
+++ camera.js
@@ -0,0 +1,51 @@
+
+
+
+
+function hasGetUserMedia() {
+  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+var errorCallback = function(e) {
+  console.log('Reeeejected!', e);
+};
+
+function snapshot() {
+  var video = document.querySelector('video');
+  var canvas = document.getElementById('camcanvas');
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(video, 0, 0);
+  loadDifferentImage(canvas.toDataURL('image/webp'))
+}
+
+// Not showing vendor prefixes.
+if(hasGetUserMedia()) {
+getUserMediaCrossBrowser = (navigator.getUserMedia || navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+navigator.webkitGetUserMedia({
+  video: {
+    optional: [{sourceId: 0}]
+  }, 
+  audio: false
+  }, 
+  
+  function(localMediaStream) {
+  var video = document.querySelector('video');
+  video.src = window.URL.createObjectURL(localMediaStream);
+
+  video.addEventListener('click', snapshot, false);
+  setInterval(snapshot, 100);
+
+  // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
+  // See crbug.com/110938.
+  video.onloadedmetadata = function(e) {
+    console.log("Camera going!"); 
+  };
+}, errorCallback);
+
+}
+
+
+
